1256282f53b3861bc4a7cceb03918565d7fe6159
i-yamaguchi <i-yamaguchi@yamaguchikakunomacbook-puro.local>
2019-08-14 00:08:43 +0900

    pom.xmlを修正

--- SampleApi/src/main/java/com/example/domain/Item.java
+++ SampleApi/src/main/java/com/example/domain/Item.java
@@ -2,11 +2,27 @@ package com.example.domain;
 
 import java.io.Serializable;
 
-import lombok.Data;
-
-@Data
 public class Item implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6089367564784299884L;
 	
 	private String name;
 	private Integer price;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	
 }
